57b1fcc636e89f46343bc29aff4ba7caf135f26c
imelchenko61@gmail.com <imelchenko61@gmail.com>
2021-06-09 16:20:48 +0200

    all is done

---
+++ HandwritingWeb/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.HandwritingView, name='HandwritingView'),
+    path('answer/', views.ResultView, name='ResultView')
+]
